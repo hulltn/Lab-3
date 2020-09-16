92ea38155889df65ca52a9ca5123baae573cdc6c
hulltn <hulltn@mail.uc.edu>
2020-09-16 03:17:44 +0000

    Count up and down

--- Main.java
+++ Main.java
@@ -1,7 +1,24 @@
 import java.util.Scanner;
+
+import javax.lang.model.util.ElementScanner6;
 class Main {
-  Scanner s = new Scanner(System.in);
-  public static void main(String[] args) {
-    System.out.println("Hello world!");
+    public static void main(String[] args) {
+    Scanner s = new Scanner(System.in);
+    int numInput; 
+    System.out.println("Enter a number to count down or up from:");
+    numInput= s.nextInt();
+    if (numInput <=0)
+    {for(int count = numInput; count <=0; count++)
+    {
+      System.out.println(count);
+    }
+    }
+    else if (numInput >=0)
+    {for(int count = numInput; count >=0; count--)
+    {
+      System.out.println(count);
+    }
+    
+    }
   }
 }
